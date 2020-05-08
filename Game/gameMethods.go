f7c9b5327147a4290f4cbafe8769145769f1daed
Nargles <jvschwimmer@gmail.com>
2020-05-08 16:07:15 -0400

    Add files via upload

---
+++ Game/gameMethods.go
@@ -0,0 +1,12 @@
+package main
+
+//Game defines a new data structure for a game
+type Game struct {
+	ID       int      `json:"id"`
+	Teams    []string `json:"Teams"`
+	Players1 []string `json:"Players1"`
+	Players2 []string `json:"Players2"`
+}
+
+//Games initialises a list of games
+type Games []Game
