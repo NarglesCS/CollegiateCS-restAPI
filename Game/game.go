671cecb6f6e13af93b0a9485edb91a26ec8961b2
Nargles <jvschwimmer@gmail.com>
2020-05-09 18:08:23 -0400

    Add files via upload

--- Game/game.go
+++ Game/game.go
@@ -1,21 +1,60 @@
 package game
 
-import "strings"
+import (
+	"strings"
+	"team"
+)
 
-//Game defines a new data structure for a game
+var person team.Player
+
+//Game defines a new data structure for a single game (Bo1)
 type Game struct {
-	ID       int      `json:"id"`
-	Teams    []string `json:"Teams"`
-	Players1 []string `json:"Players1"`
-	Players2 []string `json:"Players2"`
+	GameID  int         `json:"GameID"`
+	Teams   []string    `json:"Teams"`
+	Map     string      `json:"Map"`
+	Roster1 team.Roster `json:"Roster1"`
+	Roster2 team.Roster `json:"Roster2"`
 }
 
-//Games initialises a list of games
+//Games is a list of games. Added just in case but the only use should be in Setmatch stuct
 type Games []Game
+
+//Setmatch initialises a Bo3 or Bo5
+type Setmatch struct {
+	MatchID  int      `json:"MatchID"`
+	MapList  []string `json:"MapList"`
+	GameList []Game   `json:"GameList"`
+}
+
+//Setmatches was created just in case i would need it. But will probably be unused ~~Unused atm~~
+type Setmatches []Setmatch
+
+//GameNum method to get id value
+func (g *Game) GameNum() int {
+	return (g.GameID)
+}
+
+//GameNum for type Games
+func (g *Setmatch) GameNum() []int {
+	var id []int
+	for _, item := range g.GameList {
+		id = append(id, item.GameNum())
+	}
+	return (id)
+}
 
 //Match method to report teams ({TEAM 1} vs {TEAM 2})
 func (g *Game) Match() string {
 	return (string(g.Teams[0]) + " vs. " + string(g.Teams[1]))
+}
+
+//Match method to report teams for Games datatype
+func (g *Setmatch) Match() []string {
+	var matches []string
+	for _, item := range g.GameList {
+		matches = append(matches, item.Match())
+	}
+	return (matches)
 }
 
 //Team1 method to report team1
@@ -28,18 +67,15 @@ func (g *Game) Team2() string {
 	return (string(g.Teams[1]))
 }
 
-//Players method to report players based on team selection (TEAM 1 should always equal Players 1)
-func (g *Game) Players(team int) []string {
-	if team == 1 {
-		return (g.Players1)
-	} else if team == 2 {
-		return (g.Players2)
+//Roster method to report players based on team selection (TEAM 1 should always equal Players 1)
+func (g *Game) Roster(tm int) team.Roster {
+	if tm == 1 {
+		return (g.Roster1)
+	} else if tm == 2 {
+		return (g.Roster2)
 	}
-	//The ... is important because append is a variadic function. Research says thats any function that accepts variable amounts of inputs.
-	//It seems that since append normally accepts a slice and a single input or several single inputs. There is no issue.
-	//This is because it collects the individual inputs and creates a new slice.
-	//But, the ... denotes that it is already a slice and passes it on to the other methods behind the scenes. Or else there will be an error.
-	return (append(g.Players1, g.Players2...))
+	// returns roster 1 by default until I build a teamID == teamID logic statment
+	return g.Roster1
 }
 
 //Output a []string type as a single string deliminated with a " " character.
